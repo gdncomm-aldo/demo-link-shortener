1401eedb19fa42a4fc23a71b01d534f4df3a5aa4
aldogdn <aldo@gdn-commerce.com>
2025-11-24 20:03:10 +0700

    show exception handler

--- src/main/java/com/example/demo_link_shortener/controller/ErrorController.java
+++ src/main/java/com/example/demo_link_shortener/controller/ErrorController.java
@@ -1,10 +1,14 @@
 package com.example.demo_link_shortener.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.example.demo_link_shortener.exception.DataNotFoundException;
 import com.example.demo_link_shortener.exception.PageNotFoundException;
 
 @ControllerAdvice
@@ -14,5 +18,11 @@ public class ErrorController {
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public String handleNotFound(PageNotFoundException pageNotFoundException) {
     return "error";
+  }
+
+  @ExceptionHandler(DataNotFoundException.class)
+  public ResponseEntity<Map<String, Object>> handleNotFound(DataNotFoundException dataNotFoundException) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+      .body(Map.of("code", HttpStatus.NOT_FOUND.value(), "status", HttpStatus.NOT_FOUND.name()));
   }
 }
--- src/main/java/com/example/demo_link_shortener/controller/LinkController.java
+++ src/main/java/com/example/demo_link_shortener/controller/LinkController.java
@@ -3,6 +3,7 @@ package com.example.demo_link_shortener.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ import com.example.demo_link_shortener.command.model.CreateLinkCommandRequest;
 import com.example.demo_link_shortener.command.model.GetAllLinkCommandRequest;
 import com.example.demo_link_shortener.controller.model.LinkRequest;
 import com.example.demo_link_shortener.controller.model.LinkResponse;
+import com.example.demo_link_shortener.exception.DataNotFoundException;
+import com.example.demo_link_shortener.repository.LinkRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +27,8 @@ import lombok.RequiredArgsConstructor;
 public class LinkController {
 
   private final CommandExecutor commandExecutor;
+
+  private final LinkRepository linkRepository;
 
   @GetMapping
   public List<LinkResponse> getAll() {
@@ -44,5 +49,12 @@ public class LinkController {
     return LinkResponse.builder()
       .id(commandResponse.getId())
       .build();
+  }
+
+  @GetMapping("/{id}")
+  public LinkResponse findById(@PathVariable("id") String id) {
+    return linkRepository.findById(id)
+      .map(link -> new LinkResponse(link.getId(), link.getUrl()))
+      .orElseThrow(()-> new DataNotFoundException());
   }
 }
