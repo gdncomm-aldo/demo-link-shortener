30f156bd7f71059421c58925643582fad89a6e30
braja_tannady <braja.tannady@gdn-commerce.com>
2025-11-25 12:31:51 +0700

    add around

--- src/main/java/com/example/demo_link_shortener/aspect/CommandLoggingAspect.java
+++ src/main/java/com/example/demo_link_shortener/aspect/CommandLoggingAspect.java
@@ -2,8 +2,11 @@ package com.example.demo_link_shortener.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -17,10 +20,10 @@ public class CommandLoggingAspect {
   //  @Pointcut("execution(* com.example.demo_link_shortener.command..*.execute(..))")
 
   //  semua method dari semua class di package
-  //  @Pointcut("within(com.example.demo_link_shortener.controller..*)")
+  @Pointcut("within(com.example.demo_link_shortener.controller..*)")
 
   //  semua method di controller yang namanya dimulai "get"
-  @Pointcut("execution(* com.example.demo_link_shortener.controller..*.get*(..))")
+  //  @Pointcut("execution(* com.example.demo_link_shortener.controller..*.get*(..))")
 
   // semua method yang menggunakan anotasi Logging
   //  @Pointcut("@annotation(com.example.demo_link_shortener.annotation.Logging)")
@@ -30,6 +33,11 @@ public class CommandLoggingAspect {
   @Before("executeCommandPointcut()")
   public void logBeforeExecute(JoinPoint joinPoint) {
     log.info("{} executing {}", getCommandName(joinPoint), joinPoint.getSignature().getName());
+  }
+
+  @After("executeCommandPointcut()")
+  public void logAfterExecute(JoinPoint joinPoint) {
+    log.info("{} done executing {}", getCommandName(joinPoint), joinPoint.getSignature().getName());
   }
 
   @AfterReturning(pointcut = "executeCommandPointcut()", returning = "result")
@@ -45,6 +53,31 @@ public class CommandLoggingAspect {
     log.error("{} failed to execute with error: {}",
         getCommandName(joinPoint),
         throwable.getMessage());
+  }
+
+  @Around("executeCommandPointcut()")
+  public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
+    String commandName = "#" + pjp.getTarget().getClass().getSimpleName();
+    String method = pjp.getSignature().getName();
+
+    long start = System.currentTimeMillis();
+    log.info("{} starting {}", commandName, method);
+
+    try {
+      Object result = pjp.proceed();
+
+      long timeTaken = System.currentTimeMillis() - start;
+      log.info("{} successfully executed {} in {} ms with result: {}",
+          commandName, method, timeTaken, result);
+
+      return result;
+    } catch (Throwable ex) {
+      long timeTaken = System.currentTimeMillis() - start;
+      log.error("{} failed executing {} in {} ms with error: {}",
+          commandName, method, timeTaken, ex.getMessage());
+
+      throw ex;
+    }
   }
 
   private String getCommandName(JoinPoint joinPoint) {
