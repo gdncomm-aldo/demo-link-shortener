5189a6f71dbe678f453e52765b4016007fa1801b
braja_tannady <braja.tannady@gdn-commerce.com>
2025-11-26 02:37:23 +0700

    spring aop

--- src/main/java/com/example/demo_link_shortener/aspect/CommandLoggingAspect.java
+++ src/main/java/com/example/demo_link_shortener/aspect/CommandLoggingAspect.java
@@ -1,6 +1,6 @@
 package com.example.demo_link_shortener.aspect;
 
-import lombok.extern.slf4j.Slf4j;
+import com.example.demo_link_shortener.command.model.GetAllLinkCommandResponse;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -12,73 +12,82 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+
 @Aspect
 @Slf4j
 @Component
 public class CommandLoggingAspect {
-  //  semua method bernama execute
-  //  @Pointcut("execution(* com.example.demo_link_shortener.command..*.execute(..))")
+  // menargetkan method execute pada GetAllLinkCommandImpl
+  @Pointcut("execution(* com.example.demo_link_shortener.command.GetAllLinkCommandImpl.execute(..))")
 
-  //  semua method dari semua class di package
-  @Pointcut("within(com.example.demo_link_shortener.controller..*)")
+  // menargetkan semua class di package controller
+  // @Pointcut("within(com.example.demo_link_shortener.controller..*)")
 
-  //  semua method di controller yang namanya dimulai "get"
-  //  @Pointcut("execution(* com.example.demo_link_shortener.controller..*.get*(..))")
+  // semua method di controller yang namanya dimulai "get"
+  // @Pointcut("execution(* com.example.demo_link_shortener.controller..*.get*(..))")
 
   // semua method yang menggunakan anotasi Logging
-  //  @Pointcut("@annotation(com.example.demo_link_shortener.annotation.Logging)")
-  public void executeCommandPointcut() {
-  }
+  // @Pointcut("@annotation(com.example.demo_link_shortener.annotation.Logging)")
+  public void executeCommandPointcut() {}
 
-  @Before("executeCommandPointcut()")
-  public void logBeforeExecute(JoinPoint joinPoint) {
-    log.info("{} executing {}", getCommandName(joinPoint), joinPoint.getSignature().getName());
-  }
-
-  @After("executeCommandPointcut()")
-  public void logAfterExecute(JoinPoint joinPoint) {
-    log.info("{} done executing {}", getCommandName(joinPoint), joinPoint.getSignature().getName());
-  }
-
-  @AfterReturning(pointcut = "executeCommandPointcut()", returning = "result")
-  public void logAfterExecute(JoinPoint joinPoint, Object result) {
-    log.info("{} successfully executed {} with result: {}",
-        getCommandName(joinPoint),
-        joinPoint.getSignature().getName(),
-        result);
-  }
-
-  @AfterThrowing(pointcut = "executeCommandPointcut()", throwing = "throwable")
-  public void logAfterExecuteThrow(JoinPoint joinPoint, Throwable throwable) {
-    log.error("{} failed to execute with error: {}",
-        getCommandName(joinPoint),
-        throwable.getMessage());
-  }
-
-  @Around("executeCommandPointcut()")
-  public Object logAround(ProceedingJoinPoint pjp) throws Throwable {
-    String commandName = "#" + pjp.getTarget().getClass().getSimpleName();
-    String method = pjp.getSignature().getName();
-
-    long start = System.currentTimeMillis();
-    log.info("{} starting {}", commandName, method);
-
-    try {
-      Object result = pjp.proceed();
-
-      long timeTaken = System.currentTimeMillis() - start;
-      log.info("{} successfully executed {} in {} ms with result: {}",
-          commandName, method, timeTaken, result);
-
-      return result;
-    } catch (Throwable ex) {
-      long timeTaken = System.currentTimeMillis() - start;
-      log.error("{} failed executing {} in {} ms with error: {}",
-          commandName, method, timeTaken, ex.getMessage());
-
-      throw ex;
-    }
-  }
+//  @Before("executeCommandPointcut()")
+//  public void logBeforeExecute(JoinPoint joinPoint) {
+//    log.info("@Before {} executing {}", getCommandName(joinPoint),
+//        joinPoint.getSignature().getName());
+//  }
+//
+//  @After("executeCommandPointcut()")
+//  public void logAfterExecute(JoinPoint joinPoint) {
+//    log.info("@After {} done executing {}", getCommandName(joinPoint),
+//        joinPoint.getSignature().getName());
+//  }
+//
+//  @AfterReturning(pointcut = "executeCommandPointcut()", returning = "result")
+//  public void logAfterReturningExecute(JoinPoint joinPoint, Object result) {
+//    log.info("@AfterReturning {} successfully executed {} with result: {}",
+//        getCommandName(joinPoint), joinPoint.getSignature().getName(), result);
+//  }
+//
+//  @AfterThrowing(pointcut = "executeCommandPointcut()", throwing = "throwable")
+//  public void logAfterThrowingExecute(JoinPoint joinPoint, Throwable throwable) {
+//    log.error("@AfterThrowing {} failed to execute with error: {}", getCommandName(joinPoint),
+//        throwable.getMessage());
+//  }
+//
+//  @Around("executeCommandPointcut()")
+//  public Object logAroundExecute(ProceedingJoinPoint pjp) throws Throwable {
+//    String commandName = "#" + pjp.getTarget().getClass().getSimpleName();
+//    String method = pjp.getSignature().getName();
+//
+//    long start = System.currentTimeMillis();
+//    log.info("@Around {} starting {}", commandName, method);
+//
+//    try {
+//      Object result = pjp.proceed();
+//      Object finalResult = result;
+//
+//      if (result instanceof GetAllLinkCommandResponse response) {
+//        response.setData(new ArrayList<>());
+//        log.info("@Around Result modified before return: {}", response);
+//        finalResult =  response;
+//      }
+//
+//      long timeTaken = System.currentTimeMillis() - start;
+//      log.info("@Around {} successfully executed {} in {} ms with result: {}", commandName, method,
+//          timeTaken, finalResult);
+//
+//      return finalResult;
+//    } catch (Throwable ex) {
+//      long timeTaken = System.currentTimeMillis() - start;
+//      log.error("@Around {} failed executing {} in {} ms with error: {}", commandName, method,
+//          timeTaken, ex.getMessage());
+//
+//      throw ex;
+//    }
+//  }
 
   private String getCommandName(JoinPoint joinPoint) {
     return "#" + joinPoint.getTarget().getClass().getSimpleName();
